30e39db34fbb12ff51ab0bfe0cc1491148ada2db
João Távora <joaotavora@gmail.com>
2024-12-09 03:20:14 -0600

    day6 part 1

--- day6/day6-1.cpp
+++ day6/day6-1.cpp
@@ -1,52 +1,55 @@
-#include <numeric>
-#include <string>
 #include <iostream>
+#include <numeric>
+#include <print>
+#include <string>
 #include <string_view>
 #include <vector>
-#include <map>
-#include <print>
 
 auto main() -> int {
 
-  using map_t = std::vector<std::string>;
-  using point_t = std::pair<size_t, size_t>;
-  enum direction_e {north, east, south, west};
-
-  size_t n{0};
-  map_t map{};
-  direction_e direction{north};
-  point_t pos{0, 0};
+  std::vector<std::string>          map{};
+  enum { north, east, south, west } direction{north};
+  struct { int x; int y; }          pos{0, 0};
 
   std::string buf;
-  std::string_view starter{"^>v<"};
+  constexpr std::string_view starter{"^>v<"};
   while (std::getline(std::cin, buf)) {
-    for (auto j = 0u; j < buf.size(); ++j) {
+    for (auto j = 0; j < buf.size(); ++j) {
       if (auto p = starter.find(buf[j]); p != starter.npos) {
-        direction = static_cast<direction_e>(p);
-        pos = {j, map.size()};
+        direction = static_cast<decltype(direction)>(p);
+        pos = {j, static_cast<int>(map.size())};
+        buf[j] = 'X';
       }
     }
     map.push_back(std::move(buf));
   }
+  size_t n{map.size()};
 
-  while (true) {
+  auto step = [&]() -> bool {
+    auto ret = pos;
     switch (direction) {
-    case south: {
-      
-      break;
+    case west:  ret.x--; break;
+    case east:  ret.x++; break;
+    case north: ret.y--; break;
+    case south: ret.y++; break;
     }
-    case west: {
-      break;
+    if (ret.x < 0 || ret.x >= n || ret.y < 0 || ret.y >= n)
+      return false;
+    if (map[ret.y][ret.x] == '#') {
+      direction = static_cast<decltype(direction)>((direction + 1) % 4);
+    } else {
+      map[ret.y][ret.x] = 'X';
+      pos = ret;
     }
-    case north: {
-      break;
-    }
-    case west: {
-      break;
-    }
-  }
+    return true;
+  };
 
+  while (step());
 
-  std::println("{}", std::accumulate(bitmap.begin(), bitmap.end(), 0));
-
+  std::println(
+      "{}", std::accumulate(map.begin(), map.end(), 0, [](int acc, auto &&row) {
+        return acc + std::accumulate(
+            row.begin(), row.end(), 0,
+            [](int acc, auto &&c) { return acc + ((c == 'X') ? 1 : 0); });
+      }));
 }
